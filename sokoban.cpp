cd76d32256e69585ecee0bed4507e9d15b24cea0
Doug Inman <douglas.inman@live5.co.uk>
2024-10-08 18:51:17 +0100

    game state and title screen

--- sokoban.cpp
+++ sokoban.cpp
@@ -5,11 +5,22 @@
 #include "levels.hpp"
 using namespace std;
 
+enum GAMESTATE {
+	STATE_TITLE,
+	STATE_GAME
+};
 SDLmanager sdl;
 GFX gfx;
 DPad dpad;
 const int TSIZE = 16;
 int tsetimage = 0, pimage = 0;
+GAMESTATE gamestate = STATE_TITLE;
+
+
+void switchstate(GAMESTATE state);
+void update();
+void repaint();
+
 
 struct Game {
 	int tmap = 0, playersprite = 0;
@@ -17,10 +28,12 @@ struct Game {
 	vector<int> boxes;
 
 	void init() {
+		// make sprites and maps
 		tmap = gfx.makemap(5, 5, TSIZE, tsetimage);
 		playersprite = gfx.makesprite(TSIZE, TSIZE, pimage);
 		gfx.getsprite(playersprite).src.x = TSIZE * 2;
-		levelno = 0;
+		// load map 1
+		level2map(0);
 	}
 
 	void makebox(int tx, int ty) {
@@ -110,6 +123,83 @@ struct Game {
 	}
 } game;
 
+
+struct TitleScreen {
+	const string title = "Sokoban";
+	int titleimage = 0, titlesprite = 0, playersprite = 0;
+	int dir = 2, delta = 0;
+
+	void init() {
+		if (!titleimage) {
+			titleimage = gfx.makeimage(title.size() * gfx.FONT_W, gfx.FONT_H);
+			auto& img = gfx.getimage(titleimage);
+			gfx.print(img, title, 0, 0);
+		}
+		// words
+		titlesprite = gfx.makesprite(title.size() * gfx.FONT_W, gfx.FONT_H, titleimage);
+		auto& tspr = gfx.getsprite(titlesprite);
+		tspr.pos.x = (gfx.screen.w - tspr.pos.w) / 2;
+		tspr.pos.y = (gfx.screen.h / 2) - 20;
+		// animated sprite
+		playersprite = gfx.makesprite(TSIZE, TSIZE, pimage);
+		auto& spr = gfx.getsprite(playersprite);
+		spr.pos.x = (gfx.screen.w - spr.pos.w) / 2;
+		spr.pos.y = (gfx.screen.h / 2) + 2;
+		// boxes
+		int box1sprite = gfx.makesprite(TSIZE, TSIZE, tsetimage);
+		auto& box1 = gfx.getsprite(box1sprite);
+		box1.src.x = TSIZE * 4;
+		box1.pos.y = (gfx.screen.h / 2) + 2;
+		box1.pos.x = (gfx.screen.w - 45 - TSIZE*2) / 2;
+		int box2sprite = gfx.makesprite(TSIZE, TSIZE, tsetimage);
+		auto& box2 = gfx.getsprite(box2sprite);
+		box2.src = box1.src;
+		box2.pos.y = box1.pos.y;
+		box2.pos.x = (gfx.screen.w + 45) / 2;
+	}
+
+	// void paint() {
+	// 	gfx.fill(0xff000000);
+	// 	gfx.print( title, (gfx.screen.w - tspr.pos.w) / 2, (gfx.screen.h / 2) - 20 );
+	// 	// boxes
+	// 	Rect src = { TSIZE * 4, 0, TSIZE, TSIZE };
+	// 	gfx.blit( tsetimage, (gfx.screen.w - 45 - TSIZE*2) / 2, (gfx.screen.h / 2) + 2, src );
+	// 	gfx.blit( tsetimage, (gfx.screen.w + 45) / 2, (gfx.screen.h / 2) + 2, src );
+	// }
+
+	void update() {
+		// animate spinning guy
+		delta++;
+		if (delta > 20) {
+			dir = (dir + 1) % 4;
+			delta = 0;
+		}
+		auto& spr = gfx.getsprite(playersprite);
+		spr.src.x = dir * TSIZE;
+
+		// start game on button
+		if (dpad.a == DPad::KDOWN)  switchstate(STATE_GAME);
+	}
+} titlescreen;
+
+
+void switchstate(GAMESTATE state) {
+	gfx.tilemaps = {};
+	gfx.sprites = {};
+	gamestate = state;
+	switch (gamestate) {
+		case STATE_TITLE:  titlescreen.init();  break;
+		case STATE_GAME:   game.init();  break;
+	}
+}
+
+void update() {
+	switch (gamestate) {
+		case STATE_TITLE:  titlescreen.update();  break;
+		case STATE_GAME:   game.update();  break;
+	}
+}
+
 void repaint() {
 	// repaint game scene
 	gfx.fill(0xff000000);
@@ -121,17 +211,18 @@ void repaint() {
 int main(int argc, char* args[]) {
 	sdl.init();
 	gfx.init(160, 160);
-	gfx.flag_hit = 1;
+	// gfx.flag_hit = 1;
 
 	tsetimage = sdl.makebmp(gfx, "tiles.bmp");
 	pimage = sdl.makebmp(gfx, "player.bmp");
-	game.init();
-
-	// load map 1
-	game.level2map(0);
+	// game.init();
+	// titlescreen.init();
+	switchstate(STATE_TITLE);
 
 	while (!sdl.quit) {
-		game.update();
+		// titlescreen.update();
+		// game.update();
+		update();
 		repaint();
 	}
 
